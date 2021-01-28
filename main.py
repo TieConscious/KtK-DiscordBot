5144a3bb494c29e362204a17832659e4b1d8ff96
moo <moo@staff.42.us.org>
2021-01-28 13:30:22 -0800

    first test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import asyncio
 
 from dotenv import load_dotenv
-from writeToSheets import recordNewPlaytester, checkNewPlaytester
+from writeToSheets import recordNewPlaytester, checkNewPlaytester, findSteamKey
 from datetime import date, datetime
 
 load_dotenv()
@@ -15,63 +15,68 @@ client = discord.Client(prefix='!',intents=intents)
 def playtestCommand(message):
     user = message.author
 
-        if (checkNewPlaytester(user.name + '#' + user.discriminator) == False):
-            return await user.send("It looks like you've already joined the Guild, " + user.name + ".")
+    if (checkNewPlaytester(user.name + '#' + user.discriminator) == False):
+        return await user.send("It looks like you've already joined the Guild, " + user.name + ".")
 
-        role_id = 780498456938545242 # playtester role id
-        guild_id = 775937500643721218 # server id
-        user_answers = [datetime.now().strftime("%m/%d/%Y %H:%M"), user.name + '#' + user.discriminator]
-        opening = "Hello, traveller! Welcome to the Guild.\nIn this dark time, we're happy to have more allies.\nBut, before we can give you a key, you must answer some questions."
-        user_timeout_msg = "I see you're still unsure of whether to join or not. Come again when you're ready.\n"
-        closing = "You'll be a powerful ally, " + user.name + ". We take pride in you joining us. Here's your key: "
+    role_id = 780498456938545242 # playtester role id
+    guild_id = 775937500643721218 # server id
+    user_answers = [datetime.now().strftime("%m/%d/%Y %H:%M"), user.name + '#' + user.discriminator]
+    opening = "Hello, traveller! Welcome to the Guild.\nIn this dark time, we're happy to have more allies.\nBut, before we can give you a key, you must answer some questions."
+    user_timeout_msg = "I see you're still unsure of whether to join or not. Come again when you're ready.\n"
+    closing = "You'll be a powerful ally, " + user.name + ". We take pride in you joining us. Here's your key: "
 
-        questions = [
-            'First, a simple question. Are you able to use SteamVR?',
-            'Next, what headset do you use with SteamVR?',
-            'Last, how did you find out about "Kill the King"?'
-        ]
+    questions = [
+        'First, a simple question. Are you able to use SteamVR?',
+        'Next, what headset do you use with SteamVR?',
+        'Last, how did you find out about "Kill the King"?'
+    ]
 
-        await user.send(opening)
+    await user.send(opening)
 
-        # Ask questions and save responses
-        for question in questions:
-            def user_responded(m):
-                return m.author == user
+    # Ask questions and save responses
+    for question in questions:
+        def user_responded(m):
+            return m.author == user
 
-            await user.send(question)
-
-            try:
-                a = await client.wait_for('message', check=user_responded, timeout=45.0)
-            except asyncio.TimeoutError:
-                return await user.send(user_timeout_msg)
-            
-            if a:
-                user_answers.append(a.content)
-            else:
-                return await user.send(user_timeout_msg)
-
-        print(user_answers)
-
-        # Assign playtester role and record answers to GSheets
-        guild = client.get_guild(guild_id)
-        if guild is None:
-            return
-
-        role = guild.get_role(role_id)
-        if role is None:
-            return
+        await user.send(question)
 
         try:
-            member = guild.get_member(user.id)
-            await member.add_roles(role)
-            steam_key = recordNewPlaytester(user_answers)
-            await user.send(closing + steam_key)
-        except discord.HTTPException:
-            print("didn't work")
-            pass
+            a = await client.wait_for('message', check=user_responded, timeout=45.0)
+        except asyncio.TimeoutError:
+            return await user.send(user_timeout_msg)
+        
+        if a:
+            user_answers.append(a.content)
+        else:
+            return await user.send(user_timeout_msg)
+
+    print(user_answers)
+
+    # Assign playtester role and record answers to GSheets
+    guild = client.get_guild(guild_id)
+    if guild is None:
+        return
+
+    role = guild.get_role(role_id)
+    if role is None:
+        return
+
+    try:
+        member = guild.get_member(user.id)
+        await member.add_roles(role)
+        steam_key = recordNewPlaytester(user_answers)
+        await user.send(closing + steam_key)
+    except discord.HTTPException:
+        print("didn't work")
+        pass
 
 def resendSteamKey(message):
-    
+    user = message.author
+    key = findSteamKey(user.name + '#' + user.discriminator)
+    if key == None:
+        await user.send("Looks like you'll have to join the Guild first. Try using !playtest.")
+    else:
+        await user.send("Aye, " + user.name + ", don't go around misplacing this. Ya never know who might steal it.\nKey: " + key)
 
 @client.event
 async def on_ready():
@@ -84,7 +89,7 @@ async def on_message(message):
 
     if message.content == ('!playtest'):
         playtestCommand(message)
-    else if message.content == ('!key')
+    elif message.content == ('!key'):
         resendSteamKey(message)
 
 
--- writeToSheets.py
+++ writeToSheets.py
@@ -9,25 +9,25 @@ gc = pygsheets.authorize(service_account_env_var='GDRIVE_CREDENTIALS')
 
 # For local testing
 # gc = pygsheets.authorize(service_file='ktk-playtester-1611548901474-1547c6217fc8.json')
-user_list = set()
+user_list = {}
 
 def cacheUserList():
     spreadsheet = gc.open('Playtest Signup').sheet1
     sh_user_list = spreadsheet.get_all_values(include_tailing_empty_rows=False, include_tailing_empty=False, returnas='matrix')
     for info in sh_user_list:
-        user_list.add(info[1])
-    print(user_list)
+        # info[1] == user name, info[5] == steam key
+        user_list[info[1]] = info[5]
+    print(len(user_list))
 
 
 def checkNewPlaytester(user_name_discriminator):
     if len(user_list) == 0:
-       cachesUserList():
+       cacheUserList()
 
     # If name exists
     if user_name_discriminator in user_list:
         return False
     else:
-        user_list.add(user_name_discriminator)
         return True
 
 
@@ -36,12 +36,15 @@ def recordNewPlaytester(user_answers):
     spreadsheet = gc.open('Playtest Signup').sheet1
 
     # Update a single cell.
-    # cells = spreadsheet.get_all_values(include_tailing_empty_rows=False, include_tailing_empty=False, returnas='matrix')
     last_row = len(user_list)
     spreadsheet = spreadsheet.insert_rows(last_row, number=1, values=user_answers)
+    
+    # Add user to cache
+    return("1")
 
 
 def findSteamKey(user_name_discriminator):
     if len(user_list) == 0:
         cacheUserList()
     
+    return user_list.get(user_name_discriminator)
